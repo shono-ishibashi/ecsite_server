7bd8ca004b2c50b6d184fcba719b1836496098c4
shono-ishibashi <shono.ishibashi@rakus-partners.co.jp>
2021-01-07 16:57:41 +0900

    nginxでサーバーへの振り分けの実装
    django/ => djangoコンテナ
    auth/ => authコンテナ
    
    static ディレクトリの実装

--- django/src/api/views.py
+++ django/src/api/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+from rest_framework import status
+
+
+class HelloWorld(APIView):
+    """
+    クラスベースのAPIViewです。
+    """
+
+    def get(self, request, format=None):
+        return Response({"message": "Hello World!! this is django container"},
+                        status=status.HTTP_200_OK)
+
+    def post(self, request, format=None):
+        request_data = request.data
+        return Response({"message": request_data["message"]},
+                        status=status.HTTP_201_CREATED)
