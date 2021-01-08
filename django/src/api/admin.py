b0d412ecb35e359284faa3d6c068b41680089a72
shono-ishibashi <shono.ishibashi@rakus-partners.co.jp>
2021-01-08 15:44:41 +0900

    modelを管理に追加

--- django/src/api/admin.py
+++ django/src/api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from .models import User, Item, Topping, Order, OrderItem, OrderTopping
+
 # Register your models here.
+my_models = [User, Item, Topping, Order, OrderItem, OrderTopping]
+admin.site.register(my_models)
