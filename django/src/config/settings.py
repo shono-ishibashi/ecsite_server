63872142473da9c7d036cf72ef82d0b868e2ece3
shono-ishibashi <shono.ishibashi@rakus-partners.co.jp>
2021-01-19 15:19:09 +0900

    SECRET_KEY の変更

--- django/src/config/settings.py
+++ django/src/config/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
+from . import settings_local
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'xm#azqenx4+h=d!e5byrl2x0dt9sp%ca2l-5o$o&y_d0^*te2$'
+SECRET_KEY = settings_local.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
