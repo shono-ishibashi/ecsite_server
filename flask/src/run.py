a587a0ef307c5a9137a98c8260d5318b2fdc8065
shono-ishibashi <shono.ishibashi@rakus-partners.co.jp>
2021-01-20 15:42:56 +0900

    flask の corsの実装

--- flask/src/run.py
+++ flask/src/run.py
@@ -1,6 +1,5 @@
 from flask import Flask, jsonify
 from models import db
-from flask_cores import CORS
 
 from api import api
 
@@ -11,13 +10,10 @@ app.config.from_pyfile('conf.cfg')
 # 文字化けを解消
 app.config['JSON_AS_ASCII'] = False
 
-# CORS
-CORS(app)
-
 
 @api.after_request
 def after_request(response):
-    # response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Origin', '*')
     response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
     response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
     return response
