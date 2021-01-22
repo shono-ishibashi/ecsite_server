5cbea4d00612d3134d517b47640704a93e81618d
shono-ishibashi <66771379+shono-ishibashi@users.noreply.github.com>
2021-01-22 13:13:33 +0900

    tokenの有効期限を変更 10->59

--- auth/src/api/views.py
+++ auth/src/api/views.py
@@ -100,7 +100,7 @@ class FetchUser(APIView):
 
         # 有効期限を10分に設定
         is_valid_date = user_util.created_at > datetime.now().astimezone() - \
-            timedelta(minutes=10)
+            timedelta(minutes=59)
         if is_valid_date:
             user = models.User.objects.get(user__token=token)
             user_serializer = serializers.UserSerializer(user)
