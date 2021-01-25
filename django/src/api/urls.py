71b08e4ec7a94fa624c4a7f4b2866a46decbf6b0
shoya1007 <66931835+shoya1007@users.noreply.github.com>
2021-01-25 10:25:41 +0900

    Revert "400番台メッセージを追加"

--- django/src/api/urls.py
+++ django/src/api/urls.py
@@ -1,14 +1,7 @@
 from django.urls import path
-from django.contrib import admin
 
-
-from . import views
-
+app_name = 'api'
 
 urlpatterns = [
-    path('test/', views.request_test, name='test'),
-    path('admin/', admin.site.urls, name='admin'),
-    path('order/', views.order, name='order'),
-    path('cart/', views.cart),
-    path('delete_cart/<int:order_item_id>/', views.delete_cart)
+    path('cart/')
 ]
--- flask/src/auth_util.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-
-def fetch_login_user(token):
-    url = 'http://nginx:80/auth/user/'
-    headers = {'Authorization': token}
-    response = requests.get(url, headers=headers)
-    return response
