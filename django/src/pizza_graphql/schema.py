283bc5032516b24cb5e6c5f9d82be3ffbeafafcb
shono-ishibashi <shono.ishibashi@rakus-partners.co.jp>
2021-01-25 14:15:30 +0900

    GraphQLのディレクトリ構成の変更

--- django/src/pizza_graphql/schema.py
+++ django/src/pizza_graphql/schema.py
@@ -1,42 +1,16 @@
-import django_filters
-from django_filters import FilterSet, OrderingFilter
-
 import graphene
-from graphene_django import DjangoObjectType, DjangoListField
-
-from api.models import Item
 from graphene_django.filter import DjangoFilterConnectionField
 
-
-class ItemFilter(FilterSet):
-    class Meta:
-        model = Item
-        fields = {
-            "name": ["icontains"],
-        }
-
-    order_by = OrderingFilter(
-        fields=(
-            ('name', 'name'), ('price_m', 'price_m'),
-        )
-    )
-
-
-class ItemNode(DjangoObjectType):
-    class Meta:
-        model = Item
-        filter_fields = {
-            'name': ['exact', 'icontains', 'istartswith'],
-            'id': ['exact'],
-        }
-        interfaces = (graphene.relay.Node,)
+from pizza_graphql.my_graphql import item_ql
 
 
 class Query(graphene.ObjectType):
     # 商品一覧を取得
-    items = DjangoFilterConnectionField(ItemNode, filterset_class=ItemFilter)
+    items = DjangoFilterConnectionField(item_ql.ItemNode, filterset_class=item_ql.ItemFilter)
     # idで商品を取得
-    item = graphene.relay.Node.Field(ItemNode)
+    item = graphene.relay.Node.Field(item_ql.ItemNode)
 
 
-schema = graphene.Schema(query=Query)
+schema = graphene.Schema(
+    query=Query,
+)
